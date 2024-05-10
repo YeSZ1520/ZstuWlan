18737476dbe0a9cf044f9c3f5ab6005e5670db8a
15208 <1520813404@qq.com>
2024-05-10 12:04:02 +0800

    补充信息

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 # 教程
 ## 路由器方案
 ### OpenWrt 路由器
-1. 购买一个支持 OpenWrt 的路由器  
+1. 购买一个支持 OpenWrt 的路由器：[支持列表](http://www.ttcoder.cn/index.php/2024/04/14/support-openwrt-routers/)  
 2. 为其刷上 OpenWrt 系统
 3. 设置当中创建 Wifi  
 
@@ -17,27 +17,31 @@
 #### 登录程序
 ZSTU 使用的是锐捷 ePortal Web ，MenToHust 不可用，这里参考了 [RuijiePortalLoginTool](https://github.com/callmeliwen/RuijiePortalLoginTool) 项目
 
-我使用了 Go 重写了 RuijiePortalLoginTool 的脚本，结果如 main.go，可用性未知，感兴趣可以自己测试一下 ( 之前的代码找不到了，这是重写的，我不在学校暂时测试不了 )  
+本项目使用了 Go 重写了 [RuijiePortalLoginTool](https://github.com/callmeliwen/RuijiePortalLoginTool) 的脚本，已验证可用，可在 release 根据路由器架构进行下载    
 使用方法：  
 ```shell
 ./login 学号 密码
 ```
 
-> MTK7621 路由器编译参数：`GOOS=linux;GOARCH=mipsle;GOMIPS=softfloat;CGO_ENABLED=0
+> 自行编译需要注意路由器的编译参数
+
 
 #### 定时登录
-OpenWrt 应该自带了 Cron 定时执行程序，将登录程序加入 Cron 定时任务，早上7点执行一次即可
+OpenWrt 自带了 Cron 定时执行程序，将登录程序加入 Cron 定时任务，美团早上7点执行一次，[Cron 表达式工具](https://cron.ciding.cc/)
 
 ## 手机方案
 实际上直接使用手机连接 ZstuWlan 再开热点就能实现共享，只是这样不方便和舍友共享网络
 
-# 改进
-1. 教程不是很详细
-2. main.go 可用性未知
-3. 可添加自动填写 Cron 定时任务的功能
 
 # 问题和建议
 有问题或建议可以在 [Issues ](https://github.com/YeSZ1520/ZstuWlan/issues)提出
+
+# 常见路由器处理器架构
+
+|           处理器            |架构|
+|:------------------------:|:---:|
+|    MT7921<br>MT7620A     |MIPS|
+| MT7981B<br>MT7986A<br>MT7622B<br>IPQ8071A<br>IPQ6018<br>IPQ8072A<br>IPQ9570<br>|ARM|
 
 # 鸣谢
 [callmeliwen/RuijiePortalLoginTool](https://github.com/callmeliwen/RuijiePortalLoginTool)
--- main.go
+++ main.go
@@ -5,67 +5,72 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 )
 
 func main() {
-	// 如果接收到的参数少于2个，则打印用法并退出
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: ./ruijie_general.go username password")
-		fmt.Println("Example: ./ruijie_general.go 201620000000 123456")
+	if len(os.Args) != 3 {
+		fmt.Println("参数错误：\nlogin 账号 密码")
 		os.Exit(1)
 	}
+	fmt.Println(fmt.Sprintf("账号：[%v]", os.Args[1]))
+	fmt.Println(fmt.Sprintf("密码：[%v]", os.Args[2]))
+	loginUrl, queryString := getLoginUrl()
+	result := login(loginUrl, os.Args[1], os.Args[2], queryString)
+	fmt.Println(string(result))
+}
 
-	// 检查是否已经在线，使用 www.google.cn/generate_204 来检查在线状态
+func getLoginUrl() (string, string) {
+	// 发送 GET 请求到 204 生成器
 	resp, err := http.Get("http://www.google.cn/generate_204")
 	if err != nil {
-		fmt.Println("Error checking online status:", err)
-		os.Exit(1)
+		errHanding("204 请求失败，请检查互联网连接")
 	}
-	if resp.StatusCode == http.StatusNoContent {
-		fmt.Println("You are already online!")
-		os.Exit(0)
-	}
+	defer resp.Body.Close()
 
-	// 如果未在线，则开始锐捷认证
-	// 获取锐捷登录页面 URL
-	resp, err = http.Get("http://www.google.cn/generate_204")
-	if err != nil {
-		fmt.Println("Error fetching login page URL:", err)
-		os.Exit(1)
-	}
-	loginPageURL := resp.Request.URL.String()
-
-	// 构造登录 URL
-	loginURL := strings.Replace(loginPageURL, "index.jsp", "InterFace.do?method=login", 1)
-	fmt.Println(loginURL)
-
-	// 构造查询字符串
-	queryString := strings.Split(loginPageURL, "?")[1]
-	queryString = strings.ReplaceAll(queryString, "&", "%2526")
-	queryString = strings.ReplaceAll(queryString, "=", "%253D")
-
-	// 发送锐捷 eportal 认证请求并输出结果
-	client := &http.Client{}
-	req, err := http.NewRequest("POST", loginURL, strings.NewReader(fmt.Sprintf("userId=%s&password=%s&service=&queryString=%s&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false", os.Args[1], os.Args[2], queryString)))
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		os.Exit(1)
-	}
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36")
-	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
-	resp, err = client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending auth request:", err)
-		os.Exit(1)
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
+	if resp.StatusCode == 204 {
+		errHanding("已登录校园网")
+		return "", ""
+	} else {
+		html, err := io.ReadAll(resp.Body)
 		if err != nil {
-			os.Exit(1)
+			errHanding("读取 Http 相应失败")
 		}
-	}(resp.Body)
+		loginUrl := regexp.MustCompile(`href='([^']*)index`).FindStringSubmatch(string(html))
+		queryStrings := regexp.MustCompile(`index.jsp\?([^']*)'</script`).FindStringSubmatch(string(html))
+		queryString := strings.Replace(strings.Replace(queryStrings[1], "&", "%2526", -1), "=", "%253D", -1)
+		return loginUrl[1] + "InterFace.do?method=login", queryString
+	}
+}
 
-	fmt.Println("Auth result:", resp.Status)
+func login(loginUrl, acc, passwd, queryString string) []byte {
+	client := &http.Client{}
+	loginPostData := fmt.Sprintf("userId=%v&password=%v&service=&queryString=%v&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false", acc, passwd, queryString)
+	request, err := http.NewRequest(http.MethodPost, loginUrl, strings.NewReader(loginPostData))
+	if err != nil {
+		errHanding("无法创建req")
+	}
+
+	request.Header.Set("Accept", "*/*")
+	request.Header.Set("Accept-Encoding", "gzip, deflate")
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	request.Header.Set("Connection", "keep-alive")
+	request.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6")
+	request.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0")
+
+	resp, err := client.Do(request)
+	if err != nil {
+		errHanding("登录请求失败")
+	}
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("读取登录结果失败")
+	}
+	return respBody
+}
+
+func errHanding(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
